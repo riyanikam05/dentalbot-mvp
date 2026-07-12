235790414d4e52aab38f8d1c412aa1db0d5ec871
Riya Nikam <riyanikam005@gmail.com>
2026-07-12 21:58:20 +0530

    Recreate DentalBot with Spring Boot 3.5.16

---
+++ README.md
@@ -0,0 +1,1 @@
+# dentalbot-mvp
---
+++ src/main/java/com/riya/dentalbot/DentalbotApplication.java
@@ -0,0 +1,13 @@
+package com.riya.dentalbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentalbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentalbotApplication.class, args);
+	}
+
+}
